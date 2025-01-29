d8279bf304e61d5e5b5e05817614328c740744ce
Aasthaa21 <87704390+Aasthaa21@users.noreply.github.com>
2025-01-29 22:19:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function showPopUp() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function acceptValentine() {
+    alert("Yay! Now you're my Valentine forever! 💕");
+    closePopUp();
+}
+
+function closePopUp() {
+    document.getElementById("popup").style.display = "none";
+}
